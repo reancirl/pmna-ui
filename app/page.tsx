ed991159d33801c4c18dcc734f66941f7a62eaae
Rey <reancirl@codesquirrel.com.au>
2024-11-30 10:24:23 +0800

    remove padding top

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default async function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="p-10 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center">
         <Image
           className="dark:invert"
@@ -21,9 +21,9 @@ export default async function Home() {
         <h1 className="text-xl text-center sm:text-left font-[family-name:var(--font-geist-mono)] italic">
           Crafting the Streets, Honoring the Culture – Coming Soon.
         </h1>
-        <h2 className="text-sm text-center sm:text-left">
+        <h3 className="text-sm text-center sm:text-left">
           Stay in the Loop – Subscribe Now for Exclusive Updates and Street x Culture Offers.
-        </h2>
+        </h3>
         <div className="text-center inline-flex">
           <form>
             <input
@@ -43,7 +43,7 @@ export default async function Home() {
           </form>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center pt-10">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://www.facebook.com/alibataclothingph"
